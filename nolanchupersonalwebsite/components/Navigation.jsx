074d6bea2b4be317e27bda53e6a68c81b68ad3f8
Nolancchu <103982110+Nolancchu@users.noreply.github.com>
2023-04-24 14:36:37 -0700

    added interim background colour

---
+++ nolanchupersonalwebsite/components/Navigation.jsx
@@ -0,0 +1,34 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const Navigation = () => {
+    return (
+    <>
+          <Navbar bg={'ncc-white'} expand="lg">
+            <Navbar.Brand href="#home" className='ml-20 fs-4 flex flew-row'>
+                <img
+                  src="/React-icon.png"
+                  width="40"
+                  height="40"
+                  className="d-inline-block align-top mr-3"
+                />
+                
+                <div className='text-ncc-green text-3xl'>Nolan Chu</div>
+            </Navbar.Brand>
+    
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <div className='w-3/5'></div>
+            <Navbar id="basic-navbar-nav" className="text-2xl text-ncc-green">
+                <Nav.Link href="#home" className='mr-4'>Home</Nav.Link>
+                <Nav.Link href="#link" className='mr-4 ml-4'>Link</Nav.Link>
+                <Nav.Link href="#link" className='ml-4'>About</Nav.Link>
+            </Navbar>
+          </Navbar>
+        <div className={`w-full h-1 bg-ncc-brown`} />
+    </>
+    )
+}
+
+export default Navigation;
--- nolanchupersonalwebsite/pages/index.tsx
+++ nolanchupersonalwebsite/pages/index.tsx
@@ -1,33 +1,15 @@
 import { Inter } from 'next/font/google'
 import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Navigation from "@/components/Navigation";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="bg-ncc-white">
-      <Navbar bg={'ncc-white'} expand="lg">
-        <Navbar.Brand href="#home" className='ml-20 fs-4 flex flew-row'>
-            <img
-              src="/React-icon.png"
-              width="40"
-              height="40"
-              className="d-inline-block align-top mr-3"
-            />
-            
-            <div className='text-ncc-green text-3xl'>Nolan Chu</div>
-        </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <div className='w-3/5'></div>
-        <Navbar id="basic-navbar-nav" className="text-2xl text-ncc-green">
-            <Nav.Link href="#home" className='mr-4'>Home</Nav.Link>
-            <Nav.Link href="#link" className='mr-4 ml-4'>Link</Nav.Link>
-            <Nav.Link href="#link" className='ml-4'>About</Nav.Link>
-        </Navbar>
-      </Navbar>
-    </div>
+    <>
+      <Navigation/>
+      <div className='flex flex-column items-center w-full min-h-screen bg-ncc-green'>TEST TEXT</div>
+    </>
   )
 }
