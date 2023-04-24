8f21b30205b3700664f6d5f9509c3cd773711a29
Nolancchu <103982110+Nolancchu@users.noreply.github.com>
2023-04-24 00:31:22 -0700

    navbar version 1

--- nolanchupersonalwebsite/pages/index.tsx
+++ nolanchupersonalwebsite/pages/index.tsx
@@ -1,10 +1,7 @@
 import { Inter } from 'next/font/google'
 import "bootstrap/dist/css/bootstrap.min.css";
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { NavLink } from 'react-bootstrap';
 
 const inter = Inter({ subsets: ['latin'] })
 
