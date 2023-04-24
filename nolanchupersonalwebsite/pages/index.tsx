83149191d5c45eb2bd4c072f7110a9d605944576
Nolancchu <103982110+Nolancchu@users.noreply.github.com>
2023-04-24 00:29:08 -0700

    completed version 1 of navbar

--- nolanchupersonalwebsite/pages/index.tsx
+++ nolanchupersonalwebsite/pages/index.tsx
@@ -1,29 +1,36 @@
 import { Inter } from 'next/font/google'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-bootstrap';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <body className="bg-ncc-white">
-      <nav className="flex items-center flex-wrap bg-ncc-beige p-6">
-        <div className="flex items-center flex-shrink-0 text-ncc-brown mr-6">
-          <svg className="fill-current h-8 w-8 mr-2 ml-16" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-            <span className="font-semibold text-xl tracking-tight">Nolan Chu</span>
-        </div>
-        <div className="w-full flex flex-grow flex-row lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
-            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-ncc-brown hover:text-white mr-4">
-              Work
-            </a>
-            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-ncc-brown hover:text-white mr-4">
-              Experience
-            </a>
-            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-ncc-brown hover:text-white">
-              Profile
-            </a>
-          </div>
-        </div>
-      </nav>
-      <div className="flex flex-col items-center bg-ncc-white"></div>
-    </body>
+    <div className="bg-ncc-white">
+      <Navbar bg={'ncc-white'} expand="lg">
+        <Navbar.Brand href="#home" className='ml-20 fs-4 flex flew-row'>
+            <img
+              src="/React-icon.png"
+              width="40"
+              height="40"
+              className="d-inline-block align-top mr-3"
+            />
+            
+            <div className='text-ncc-green text-3xl'>Nolan Chu</div>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <div className='w-3/5'></div>
+        <Navbar id="basic-navbar-nav" className="text-2xl text-ncc-green">
+            <Nav.Link href="#home" className='mr-4'>Home</Nav.Link>
+            <Nav.Link href="#link" className='mr-4 ml-4'>Link</Nav.Link>
+            <Nav.Link href="#link" className='ml-4'>About</Nav.Link>
+        </Navbar>
+      </Navbar>
+    </div>
   )
 }
