df27a4b516580a5ddc859fff216da82b47a7720d
Nolancchu <103982110+Nolancchu@users.noreply.github.com>
2023-04-26 14:52:09 -0700

    finished draft of HomePageGraphic

--- nolanchupersonalwebsite/components/HomePageGraphic.jsx
+++ nolanchupersonalwebsite/components/HomePageGraphic.jsx
@@ -5,7 +5,7 @@ import { Bebas_Neue } from "next/font/google";
 
 const garamond = Cormorant_Garamond({
     subsets: ['latin'], 
-    weight: ['400'],
+    weight: ['300'],
 })
 
 const Bebas = Bebas_Neue ({
@@ -21,23 +21,23 @@ const HomePageGraphic = () => {
         </Head>
         <div className="flex flex-row overflow-clip w-full bg-ncc-beige">
             <div className="col mt-10 ml-16 w-1/2 z-10">
-                <div className="text-9xl translate-y-[2.7rem]">
+                <div className="text-9xl translate-y-[2.6rem]">
                     <span className={garamond.className}>PRE</span>
                 </div>
                 <div className="text-9xl">
                     <span className={garamond.className}>PRETEN</span>
                 </div>
-                <div className="text-9xl -translate-y-[2.1rem]">
+                <div className="ml-[.4rem] text-9xl -translate-y-[2rem]">
                     <span className={Bebas.className}>PRETENTIOUS TEXT</span>
                 </div>
-                <div className="text-xl -translate-y-[3rem]">
+                <div className="ml-3 text-xl -translate-y-[3rem]">
                     <span className={garamond.className}>My name is Nolan Chu, a first-year computer science <br></br> student at the University of California, Riverside.</span>
-                </div>
+                </div>            
             </div>
             <img
                     src="/React-icon.png"
-                    width={80}
-                    className="-translate-y-96 h-full align-middle"
+                    width={600}
+                    className="h-full align-middle translate-y-0"
                   />
         </div>
     </div>
