e484b3f50cc8cc213177c0d52f7daaeedee85125
Nolancchu <103982110+Nolancchu@users.noreply.github.com>
2023-04-25 19:31:22 -0700

    version 2 of footer

--- nolanchupersonalwebsite/components/Footer.jsx
+++ nolanchupersonalwebsite/components/Footer.jsx
@@ -9,34 +9,39 @@ const Footer = () => {
   return (
     <footer className="w-full relative">
         <div className={`w-full h-1 bg-ncc-brown`} />
-        <div className="w-full relative bottom-0 bg-ncc-white">
-          <div className="h-24 flex flex-row justify-center items-center">
+        <div className="w-full flex flex-col items-center  bg-ncc-beige">
+            <div className="h-14 mt-3 flex flex-row justify-center items-center">
 
-            <motion.a
+                <motion.a
+                    target="_blank"
+                    href="https://discord.gg/Ugjp6Us"
+                    className="text-ncc-brown hover:opacity-70 mr-8"
+                >
+                    <FaGithubSquare size={20}/>
+                </motion.a>
+
+                <motion.a
                 target="_blank"
                 href="https://discord.gg/Ugjp6Us"
-                className="text-ncc-brown hover:opacity-70 p-12"
-            >
-                <FaGithubSquare size={74}/>
-            </motion.a>
+                className="text-ncc-brown hover:opacity-70 "
+                >
+                <FaLinkedin size={20} />
+                </motion.a>
 
-            <motion.a
-              target="_blank"
-              href="https://discord.gg/Ugjp6Us"
-              className="text-ncc-brown hover:opacity-70 p-12"
-            >
-              <FaLinkedin size={74} />
-            </motion.a>
-
-            <motion.a
-              target="_blank"
-              className="text-ncc-brown hover:opacity-70 p-12"
-              href="https://ieeeucr.github.io/subscribe.html"
-            >
-              <GrMail size={84} />
-            </motion.a>
+                <motion.a
+                target="_blank"
+                className="text-ncc-brown hover:opacity-70 ml-8"
+                href="https://ieeeucr.github.io/subscribe.html"
+                >
+                <GrMail size={20} />
+                </motion.a>
             
-          </div>
+            </div>
+
+            <div className="text-ncc-brown mb-10">
+                Nolan Christopher Chu
+            </div>
+
         </div>
     </footer>
   );
